f0ddbb4cdb6080e02bb847089c2c29f7ab31832b
MrPhoenix <nieva.cronos@gmail.com>
2025-06-22 20:17:25 -0300

    updaptes 0.0.1

--- 01-JavaScriptDesdeCero/CLAE-3-Estructura/02_Arrays.js
+++ 01-JavaScriptDesdeCero/CLAE-3-Estructura/02_Arrays.js
@@ -1,7 +1,6 @@
 let arr=[1,2,3,4,5,'true',6];
        //0-1-2-3-4-5-6
 
-
     //  iNDEX OFF  -nos encuentra el elemento que queremos buscar en la direccion de memoria  
 // console.log(arr.indexOf('true'));
 
@@ -11,9 +10,39 @@ let arr=[1,2,3,4,5,'true',6];
 // arr.push('javascript','desde','cero');
 // console.log(arr);
 // arr.unshift('javascript','desde','cero');
-console.log(arr.length);
-let newLength =arr.unshift('javascript','desde','cero');
+// console.log(arr.length);
+// let newLength =arr.unshift('javascript','desde','cero');
+// console.log(arr);
+// console.log(newLength);
+
+
+
+// Eliminar un solo elemento sin parametros
+// Estos metodos deveuelven el elemento eliminado
+// .pop()al final y .shift()al inicio
+
+// console.log(arr);
+// let deletedEl=arr.pop();
+// console.log(arr);
+// console.log(deletedEl);
+
+
+// .join('separador')
+// 'separador' es por defecto una coma;
+// console.log(arr.join(','))
+
+
+// splice(a,b,items)
+// añade o quita elementos
+// console.log(arr);
+// arr.splice(2,2);//elimina2elementos
+// arr.splice(2,2,'escuel','amigo','digital');//añade 2 elementos, eliminando si agrego mas
+// arr.splice(2,0,'escuela','amigo','digital');//añade elementos 0 sin eliminar nada 
+
+let arr2=arr.slice(0,5);//es un nuevo array no es el mismo es una copia
+// let arr3=arr esto e suna referencia , no e suna copia sigue dependiendo de array original 
 console.log(arr);
-console.log(newLength);
+console.log(arr2);
+
 
 
